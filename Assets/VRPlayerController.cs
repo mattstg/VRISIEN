2b38a7603dc62a7da7ae95cb5564b60e771c54f7
Pansimula <54655619+Pansimula@users.noreply.github.com>
2020-01-08 16:47:50 -0500

    Initial commit

---
+++ Assets/VRPlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRPlayerController : MonoBehaviour
+{
+    public Transform uiDebugParent;
+    Rigidbody rb;
+    Dictionary<string, Text> nameToUITextDict = new Dictionary<string, Text>();
+    // Start is called before the first frame update
+    string[] axises = new string[]
+    {
+          "Oculus_GearVR_LThumbstickX"                        ,
+          "Oculus_GearVR_LThumbstickY"                        ,
+          "Oculus_GearVR_RThumbstickX"                        ,
+          "Oculus_GearVR_RThumbstickY"                        ,
+          "Oculus_GearVR_DpadX"                               ,
+          "Oculus_GearVR_DpadY"                               ,
+          "Oculus_GearVR_LIndexTrigger"                       ,
+          "Oculus_GearVR_RIndexTrigger"                       ,
+          "Oculus_CrossPlatform_PrimaryIndexTrigger"          ,
+          "Oculus_CrossPlatform_SecondaryIndexTrigger"        ,
+          "Oculus_CrossPlatform_PrimaryHandTrigger"           ,
+          "Oculus_CrossPlatform_SecondaryHandTrigger"         ,
+          "Oculus_CrossPlatform_PrimaryThumbstickHorizontal"  ,
+          "Oculus_CrossPlatform_PrimaryThumbstickVertical"    ,
+          "Oculus_CrossPlatform_SecondaryThumbstickHorizontal",
+          "Oculus_CrossPlatform_SecondaryThumbstickVertical"
+    };
+
+    string[] ovrbuttons = new string[]
+    {
+          "Oculus_CrossPlatform_Button2",               
+          "Oculus_CrossPlatform_Button4",               
+          "Oculus_CrossPlatform_PrimaryThumbstick",     
+          "Oculus_CrossPlatform_SecondaryThumbstick"    
+    };
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        GameObject textResource = Resources.Load<GameObject>("TestText");
+        foreach (string s in axises)
+        {
+            GameObject go = GameObject.Instantiate(textResource, uiDebugParent);
+            nameToUITextDict.Add(s,go.GetComponent<Text>());
+            nameToUITextDict[s].text = s.Replace("Oculus_", "").Replace("GearVR_", "").Replace("CrossPlatform_", "");
+        }
+        foreach (string s in ovrbuttons)
+        {
+            GameObject go = GameObject.Instantiate(textResource, uiDebugParent);
+            nameToUITextDict.Add(s, go.GetComponent<Text>());
+            nameToUITextDict[s].text = s.Replace("Oculus_", "").Replace("GearVR_", "").Replace("CrossPlatform_", "");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(KeyValuePair<string,Text> kv in nameToUITextDict)
+        {
+            kv.Value.text = kv.Key.Replace("Oculus_", "").Replace("GearVR_", "").Replace("CrossPlatform_", "") + ": \n" + Input.GetAxis(kv.Key);
+        }
+        //transform.position = transform.position + Vector3.up;
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.velocity = new Vector3(Input.GetAxis("Oculus_GearVR_LThumbstickX"), 0, Input.GetAxis("Oculus_GearVR_LThumbstickY"));
+        rb.MovePosition(new Vector3(10, 10, 10));
+    }
+}
---
+++ Assets/rotatecam.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatecam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float x;
+    public Vector3 rot;
+    float clampedAngles = 45; //was 70
+    // Update is called once per frame
+    void Update()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+        float d = 0;
+        float sideways = 0;
+        if (Input.GetKey(KeyCode.W))
+            d = 20;
+        else if (Input.GetKey(KeyCode.S))
+            d = -20;
+
+        if (Input.GetKey(KeyCode.A))
+            sideways = 20;
+        else if (Input.GetKey(KeyCode.D))
+            sideways = -20;
+        
+        transform.Rotate(new Vector3(d*Time.deltaTime, 0, 0));
+       // rot = transform.localRotation.eulerAngles;
+       // var offset = 180f;
+        x = transform.eulerAngles.x;
+        rot = transform.eulerAngles;
+        if (x < 180)
+            x = Mathf.Clamp(x, 0, clampedAngles);
+        else
+            x = Mathf.Clamp(x, 360 - clampedAngles, 360);
+       // x = Mathf.Clamp(x, offset-70, offset + 70);
+       // if (x < 360)
+       //     x = -360 + x; 
+       //  rot.x = x;
+        transform.eulerAngles = new Vector3(x,transform.eulerAngles.y + sideways*Time.deltaTime ,transform.eulerAngles.z);
+
+    }
+}
