ae8f4279c17614db172941cc7e04e0816b26e9fb
jeelpatelofficial <jeelpatel8460553028@gmail.com>
2020-01-11 19:57:30 -0500

    gg
    ggfdfdfd

---
+++ Assets/Scripts/RangedEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedEnemy : MonoBehaviour
+{
+    public Transform gunPoint;
+
+    GameObject player;
+
+    bool isInCover = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RayCast();
+        SetRotation();
+    }
+
+    void RayCast()
+    {
+        Debug.DrawLine(gunPoint.transform.position, player.transform.position,Color.blue);
+    }
+
+    void SetRotation()
+    {
+        if(isInCover)
+        {
+            transform.LookAt(player.transform.position);
+        }
+    }
+}
