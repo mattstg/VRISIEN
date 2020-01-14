d279819e9237d41352cf9dc1246568f778aafe59
jeelpatelofficial <jeelpatel8460553028@gmail.com>
2020-01-14 13:45:36 -0500

    BaseDOne

--- Assets/Scripts/RangedEnemy.cs
+++ Assets/Scripts/RangedEnemy.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class RangedEnemy : MonoBehaviour
@@ -7,19 +8,33 @@ public class RangedEnemy : MonoBehaviour
     public Transform gunPoint;
 
     GameObject player;
+    GameObject[] coverObjects;
+    Vector3 coverLocation;
+    NavMeshAgent nv;
 
-    bool isInCover = true;
+    bool isFoundCover = false;
+    bool isInCover = false;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        coverObjects = GameObject.FindGameObjectsWithTag("CoverLocation");
+        nv = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
         RayCast();
-        SetRotation();
+
+        if (!isFoundCover)
+            FindCover();
+        if (isFoundCover)
+            if (!isInCover)
+                MoveToCover();
+            else
+                ShootPlayer();
     }
 
     void RayCast()
@@ -27,11 +42,46 @@ public class RangedEnemy : MonoBehaviour
         Debug.DrawLine(gunPoint.transform.position, player.transform.position,Color.blue);
     }
 
-    void SetRotation()
+    void FindCover()
     {
-        if(isInCover)
+        float nearestCoverDistance = 0;
+        Vector3 nearestCoverLocation = Vector3.zero;
+        for(int i=0;i < coverObjects.Length;i++)
         {
-            transform.LookAt(player.transform.position);
+            if (i == 0)
+            {
+                nearestCoverLocation = coverObjects[i].transform.position;
+                nearestCoverDistance = Vector3.Distance(transform.position, coverObjects[i].transform.position);
+            }
+            else
+            {
+                if (Vector3.Distance(transform.position, coverObjects[i].transform.position) < nearestCoverDistance)
+                {
+                    nearestCoverLocation = coverObjects[i].transform.position;
+                    nearestCoverDistance = Vector3.Distance(transform.position, coverObjects[i].transform.position);
+                }
+            }
         }
+        coverLocation = nearestCoverLocation;
+        isFoundCover = true;
+    }
+
+    void MoveToCover()
+    {
+        if (Vector3.Distance(transform.position, coverLocation) > 1f)
+            nv.SetDestination(coverLocation);
+        else
+            isInCover = true;
+    }
+
+    void ShootPlayer()
+    {
+        RotateTowardsPlayer();
+        print("Shooting");
+    }
+
+    void RotateTowardsPlayer()
+    {
+        transform.LookAt(player.transform);
     }
 }
