f8849c82174fe6af144d5d65e02aa7ba6f35fb63
chimera279 <55556029+chimera279@users.noreply.github.com>
2020-01-15 02:05:31 -0500

    Random gravity script added to grabbables
    Simulate fluid throw and catch. Must test amount to ideality.

---
+++ Assets/randomGravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomGravity : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(Vector3.down * .5f);
+        if (Input.GetKey(KeyCode.Space))
+            rb.AddExplosionForce(10f,Vector3.one,60f);
+    }
+}
