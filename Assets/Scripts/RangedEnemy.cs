7a807cbe1844052655ba56f62b96c74412ceff62
SaiGow <55634956+SaiGowReg@users.noreply.github.com>
2020-01-16 15:47:34 -0500

    Changes to sameer's work
    deleted

--- Assets/Scripts/RangedEnemy.cs
+++ Assets/Scripts/RangedEnemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine;
 
-public class RangedEnemy : MonoBehaviour,IHittable
+public class RangedEnemy : MonoBehaviour
 {
     public Transform gunPoint;
 
