ef2249029b6d91053567fd5a6ae7787569ca1df7
Sai Gowtham Regulagadda <sregulag@isimtl.local>
2020-01-16 08:05:03 -0500

    Collectable stuff
    Rigged collectables and put them in temp locations for the demo

---
+++ Assets/Resources/Scripts/Architecture/Managers/CollectableManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableManager
+{
+
+    #region Singleton
+    private static CollectableManager instance;
+    private CollectableManager() { }
+    public static CollectableManager Instance { get { return instance ?? (instance = new CollectableManager()); } }
+    #endregion
+
+    public void Initialize()
+    {
+
+    }
+
+    public void PostInitialize()
+    {
+
+    }
+
+    public void PhysicsRefresh()
+    {
+
+    }
+
+    public void Refresh()
+    {
+    }
+
+
+
+}
+
