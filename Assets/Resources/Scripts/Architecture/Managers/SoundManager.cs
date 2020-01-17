9686b7c13950e32d8bdac3d5a47543381140aa8d
bhumitpal <palbhumit11@gmail.com>
2020-01-17 17:24:43 -0500

    Melee Enemy Animations

---
+++ Assets/Resources/Scripts/Architecture/Managers/SoundManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    #region Singleton
+    private static SoundManager instance;
+    private SoundManager() { }
+    public static SoundManager Instance { get { return instance ?? (instance = new SoundManager()); } }
+    #endregion
+
+
+}
