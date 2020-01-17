253fa2eddcbe62ccc2f5cf8b7c727c8438f19355
chimera279 <55556029+chimera279@users.noreply.github.com>
2020-01-17 18:50:11 -0500

    Added damage collider to sword.
    Currently, script tracks velocity and angular velocity of strike. Might use it to determine damage if works well.

---
+++ Assets/Kaushik/Other Scripts/swordDamage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swordDamage : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float velocity, angularVelocity;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        velocity = rb.velocity.magnitude;
+        angularVelocity = rb.angularVelocity.magnitude;
+    }
+}
