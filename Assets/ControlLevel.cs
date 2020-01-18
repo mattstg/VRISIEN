551b9a2044e42c6f6836535c5933d92c3b606d11
chimera279 <55556029+chimera279@users.noreply.github.com>
2020-01-17 20:06:25 -0500

    Made more changes. Added controller to level state machine

---
+++ Assets/ControlLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlLevel : MonoBehaviour
+{
+    public int numEnemiesKilled;
+    public bool FoundSecretExit, SaiSaysYes, FoundCollectible, UsedCollectible, KilledBoss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            numEnemiesKilled++;
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            numEnemiesKilled--;
+
+        if (Input.GetKeyDown(KeyCode.X))         // Secret Exit
+            FoundSecretExit = !FoundSecretExit;
+        if (Input.GetKeyDown(KeyCode.S))         // Secret Exit
+            SaiSaysYes = !SaiSaysYes;
+        if (Input.GetKeyDown(KeyCode.C))         // Secret Exit
+            FoundCollectible = !FoundCollectible;
+        if (Input.GetKeyDown(KeyCode.E))         // Secret Exit
+            UsedCollectible = !UsedCollectible;
+        if (Input.GetKeyDown(KeyCode.F))         // Secret Exit
+            KilledBoss = !KilledBoss;
+    }
+}
