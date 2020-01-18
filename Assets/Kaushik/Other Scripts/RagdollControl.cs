4d9857fe98190845a6253a27f5a2760dffc97ac5
chimera279 <55556029+chimera279@users.noreply.github.com>
2020-01-17 22:41:31 -0500

    Added ragdoll to melee enemy. Made new prefab.

---
+++ Assets/Kaushik/Other Scripts/RagdollControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollControl : MonoBehaviour
+{
+    Rigidbody[] ragdollBodies;
+    public bool ragdollToggle;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        ragdollBodies = gameObject.GetComponentsInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            ragdollToggle = !ragdollToggle;
+
+        foreach (Rigidbody r in ragdollBodies)
+            r.isKinematic = !(ragdollToggle);
+        anim.enabled = !ragdollToggle;
+
+        
+    }
+}
