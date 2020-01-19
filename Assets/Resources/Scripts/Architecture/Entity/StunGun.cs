be793b8252183b6f2f50cb8b5906fb7e4002c398
chimera279 <55556029+chimera279@users.noreply.github.com>
2020-01-18 23:37:07 -0500

    Fixed Stun Gun. Called it a damn day with these damn metas. Empty folders be damned. DAMN!

--- Assets/Resources/Scripts/Architecture/Entity/StunGun.cs
+++ Assets/Resources/Scripts/Architecture/Entity/StunGun.cs
@@ -35,14 +35,14 @@ public class StunGun : MonoBehaviour
         {
             if (grabRef.grabbedByRight)
             {
-                if (OVRInput.Get(OVRInput.Button.One) && cooldownTime > timer)
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && cooldownTime > timer)
                 {
                     Shoot();
                     cooldownTime = 0;
                 }
             }
             else
-                if (OVRInput.Get(OVRInput.Button.One) && cooldownTime > timer)
+                if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) && cooldownTime > timer)
                 {
                     Shoot();
                     cooldownTime = 0;
@@ -71,7 +71,6 @@ public class StunGun : MonoBehaviour
         var go = transform.CheckRaycast();
         if (go)
         {
-            Debug.Log("SDHOWIDHE");
             StartCoroutine(Stun(go.transform.root.GetComponent<RagdollControl>()));
         }
         
