1f78ee6d41ac9dc1973d3bcee4de15d8ef5ba44c
jeelpatelofficial <jeelpatel8460553028@gmail.com>
2020-01-30 18:40:04 -0500

    Aded coverspots enabler

---
+++ Assets/CoverSpotEnabler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoverSpotEnabler : MonoBehaviour
+{
+    public GameObject coverspots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coverspots.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.CompareTag("Player"))
+        {
+            coverspots.SetActive(true);
+        }
+        Destroy(gameObject);
+    }
+}
