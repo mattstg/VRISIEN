0d5f0237e2947c298fc8246a33e0b581ed6c8acc
jeelpatelofficial <jeelpatel8460553028@gmail.com>
2020-01-29 20:17:04 -0500

    Script for Animation of Hands

---
+++ Assets/HandAnimatorVR.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimatorVR : MonoBehaviour
+{
+    OVRGrabbable grabRef;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grabRef.grabbedByRight)
+        {
+            if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+            {
+                animator.SetBool("isGrab", true);
+            }
+
+            if (OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger))
+            {
+                animator.SetBool("isGrab", false);
+            }
+        }
+        else
+        {
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                animator.SetBool("isGrab", true);
+            }
+
+            if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                animator.SetBool("isGrab", false);
+            }
+        }
+    }
+}
