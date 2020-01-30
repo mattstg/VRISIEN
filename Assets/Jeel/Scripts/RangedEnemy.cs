febae8c0eb5caa8ce0b13c5967e7c04f50a77618
jeelpatelofficial <jeelpatel8460553028@gmail.com>
2020-01-30 14:21:19 -0500

    ranged enemy disable collider on death

--- Assets/Jeel/Scripts/RangedEnemy.cs
+++ Assets/Jeel/Scripts/RangedEnemy.cs
@@ -242,6 +242,7 @@ public class RangedEnemy : Enemy, IHittable
 
     IEnumerator DeathSequence(float time)
     {
+        gameObject.GetComponent<Collider>().enabled = false;
         canReactToDamage = false;
         //Activate RagDoll
         animController.SetTrigger("Die");
