00a6b128d0d62085a38e38ff1c2f954890e32aaf
SaiGow <55634956+SaiGowReg@users.noreply.github.com>
2020-01-30 16:03:11 -0500

    Fixed bug with collectable.cs

--- Assets/Resources/Scripts/Architecture/Entity/Collectable.cs
+++ Assets/Resources/Scripts/Architecture/Entity/Collectable.cs
@@ -10,7 +10,7 @@ public class Collectable : MonoBehaviour
         Debug.Log("Collectable thing touched");
         Debug.Log("Tag of the thing is " + collision);
 
-        if (collision.collider.gameObject.CompareTag("Left")|| collision.collider.gameObject.CompareTag("Right")) //mixamorig:RightHand 
+        if (collision.gameObject.CompareTag("Left")|| collision.gameObject.CompareTag("Right")) //mixamorig:RightHand 
         {
         UIManager.Instance.SpawnUI(gameObject.transform);
             
